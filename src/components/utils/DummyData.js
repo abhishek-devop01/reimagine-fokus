79ab3e843cdaa63d8a2b0335a2fc8ef923a8d10a
abhishek devop <standup787059@gmail.com>
2025-07-25 23:27:25 +0530

    font update

--- src/components/utils/DummyData.js
+++ src/components/utils/DummyData.js
@@ -7,21 +7,21 @@ const testimonials = [
   },
   {
     name: 'Tanmay "Scout"',
-    title: 'CTO at Nimbus AI',
+    title: 'YouTuber & esports athlete',
     review: '“Being among the first to try Fokus, I knew from the first sip—this wasn’t just a drink; it was an experience! Now, I proudly say I’m its biggest fan.”',
         image: '/02.jpg'
 
   },
   {
     name: 'Purav Jha',
-    title: 'Marketing Manager at JetBloom',
+    title: 'YouTuber, actor, and content creator',
     review: '“Is 400ml ki bottle mein itni cheezein hai! Gym se lekar shoot tak, har jagah Fokus mera saath dega to keep me going all day long.”',
         image: '/03.jpg'
 
   },
   {
     name: 'Aditya Rikhari',
-    title: 'Engineering Manager at HyperLayer',
+    title: 'Singer-songwriter, Musician',
     review: '“Fokus—naam hi nahi, ek vibe hai! The branding, the taste, the whole energy of it is so addictive that it’s effortlessly becoming my daily companion.”',
         image: '/04.jpg'
 
